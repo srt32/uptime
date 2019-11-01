02b1d18ef44538efd7e9ee9d52b9c7657c5e82eb
Simon Taranto <srt32@github.com>
2019-11-01 13:55:18 -0600

    first pass at action

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # uptime
+
 GitHub Action to check the status of endpoints
+
+## Inputs
+
+### url-to-hit
+
+**Required** The url to hit
+
+### expected-statuses
+
+The list of statuses that are okay. Defaults to `[200]`
+
+## Outputs
+
+### status
+
+The status we got.
+
+## Example usage
+
+```
+users: srt32/uptime
+with:
+  url-to-hit: 'myservice.example.com'
+  expected-statues: [200, 301] 
+```
---
+++ index.js
@@ -0,0 +1,26 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const https = require('https');
+
+try {
+  const urlToHit = core.getInput('url-to-hit');
+  const expectedStatuses = core.getInput('expected-statuses');
+  console.log(`Pinging ${urlToHit} and expecting ${expectedStatuses}`);
+
+  https.get(urlToHit, (resp) => {
+    resp.on('end', () => {
+      if !expectedStatuses.includes(resp.statusCode) {
+        core.setFailed(`Request status was ${resp.statusCode}`);
+        // TODO(srt32): create an issue
+      } else {
+        console.log(`Successful check`);
+        core.setOutput("status", resp.statusCode);
+      }
+    });
+  }).on("error", (err) => {
+    console.log(`Request failed`);
+    core.setFailed(err.message);
+  })
+} catch (error) {
+  core.setFailed(error.message);
+}
